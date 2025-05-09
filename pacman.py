622300f7d9b84da41f2d97a05fa416b8d06bdd3d
AntonioLanderos <a00574000@tec.mx>
2025-05-08 19:48:38 -0600

    Aumentar velocidad de los fantasmas

--- pacman.py
+++ pacman.py
@@ -22,10 +22,10 @@ writer.hideturtle()
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
 
 # fmt: off
@@ -153,8 +153,8 @@ def move():
             point.move(course)
         else:
             plan = choice([
-                vector(5, 0), vector(-5, 0),
-                vector(0, 5), vector(0, -5),
+                vector(10, 0), vector(-10, 0),
+                vector(0, 10), vector(0, -10),
             ])
             course.x, course.y = plan.x, plan.y
 
